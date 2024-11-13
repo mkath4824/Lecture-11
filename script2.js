e797291c0e8a42d808b629aaa3150a515be01cd2
mkath4824 <kathmunoz4824@gmail.com>
2024-11-13 18:26:49 -0500

    Lab8-html

---
+++ script2.js
@@ -0,0 +1,112 @@
+//Question 1
+let age = 18;
+console.log(age)
+
+let firstName = "Kathleen";
+console.log(firstName)
+
+//Question 2
+
+let Name = "Kathy";
+console.log("String:",Name)
+
+let Age = 30;
+console.log("Number:", Age);
+
+
+let NJITStudent = true;
+console.log("Boolean:",NJITStudent);
+
+let color;
+console.log("Undefined:", color);
+
+//Question 3 
+
+let RollNumber = 15;
+if (RollNumber > 10 ){
+    house = "The RollNumber is greater than 10";
+    console.log(house);
+}
+
+//Question 4 
+
+let isMemeber = true;
+console.log("Boolean:",isMemeber);
+if (isMemeber=true ){
+    house = "Member discount applied";
+    console.log(house);
+}
+
+// Question 5
+
+let startValue = 50;
+
+if (startValue > 0){
+    console.log("THe value is positive");
+}
+
+else if (startVlaue < 0){
+    console.log("THe value is negative")
+}
+
+else{
+    console.log("The value is zero")
+}
+
+//Question 6 
+let a = 10;
+let b = 20;
+
+console.log("a == b:", a == b);
+console.log("a === b:", a === b);
+console.log("a != b:", a != b);
+console.log("a > b:", a > b);
+console.log("a < b:", a < b);
+console.log("a >= b:", a >= b);
+console.log("a <= b:", a <= b);
+
+// Question 7 
+let Firstname = "Kathleen";
+let Lastname = "Munoz";
+let AGE = 30;
+console.log("Hello," + Firstname + "" + Lastname + ", you are " + age + "years old.");
+
+// Question 8 
+let number = 50;
+if (number > 0 && number < 100){
+    console.log("The number is within range");
+}
+else {
+    console.log("The number is oout of range")
+}
+
+//Question 9
+let hasCar = True;
+let hasLicense = True;
+let hasInsurance = True;
+if (hasCar && hasLicense && hasInsurance){
+    console.log("You can drive legally.");
+}
+else{
+    console.log("Check your driving eligibility.");}
+
+//Question 10
+let num =21;
+if (num % 2 === 0){
+    console.log("The number is even.");
+}
+else{
+    console.log("The number is odd.");
+}
+
+// Question 11
+let score = 85;
+if (score>= 90){
+    console.log("Grade A");
+}
+else if(score >= 80 && score <= 89){
+    console.log("Grade B");
+}
+else{
+    console.log("Grade C");
+}
---
+++ scriptOne.js
@@ -0,0 +1,27 @@
+
+
+let house;
+
+let number = 70;
+if (number > 90 ){
+    house = "Grade A";
+    console.log(house);
+}
+
+
+else if (number > 80){
+    house = "Grade B"
+console.log(house);
+}
+
+else if (number > 70){
+    house = "Grade 7"
+    console.log(house);
+}
+
+else{
+    house = "Fail"
+    console.log(house);
+}
+
+number > 90 ? "Grade";
